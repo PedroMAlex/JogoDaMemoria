f60d0f531b9ae25758acbd01b63f097afb072b2d
Janilton Barbosa <janilton.barbosa@gmail.com>
2022-07-24 20:12:21 -0300

    ajustes visuais

--- app/src/main/java/br/com/pedroalex/jogodamemoria/view/PlayerNameActivity.java
+++ app/src/main/java/br/com/pedroalex/jogodamemoria/view/PlayerNameActivity.java
@@ -2,10 +2,12 @@ package br.com.pedroalex.jogodamemoria.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -21,10 +23,13 @@ public class PlayerNameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_name);
 
+        TextView txtNomeJogador = findViewById(R.id.txtNomeDoJogador);
         Button btnSairPlayerName = findViewById(R.id.btnSairSegundaTela);
         EditText edtNomeJogador = findViewById(R.id.edtNomeDoJogador);
         Button btnIniciarOJogo = findViewById(R.id.btnIniciarOJogo);
         activity = this;
+
+        txtNomeJogador.setShadowLayer(10, 0, 0, Color.YELLOW);
 
         btnIniciarOJogo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
--- app/src/main/java/br/com/pedroalex/jogodamemoria/view/SplashScreenActivity.java
+++ app/src/main/java/br/com/pedroalex/jogodamemoria/view/SplashScreenActivity.java
@@ -2,9 +2,11 @@ package br.com.pedroalex.jogodamemoria.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,19 +20,15 @@ public class SplashScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         Button proximo = findViewById(R.id.btn_proximo);
-        Button sair = findViewById(R.id.btn_sair);
+        TextView txtTitulo = findViewById(R.id.txt_titulo);
         activity = this;
+
+        txtTitulo.setShadowLayer(10, 0, 0, Color.YELLOW);
 
         proximo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(SplashScreenActivity.this, PlayerNameActivity.class);
                 startActivity(intent);
-                activity.finish();
-            }
-        });
-
-        sair.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
                 activity.finish();
             }
         });
